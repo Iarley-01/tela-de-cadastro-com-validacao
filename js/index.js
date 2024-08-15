292e35acac3e48f40fa197da44aafaa930b10699
Iarley-01 <yarleyalves2016@gmail.com>
2024-08-15 14:52:26 -0300

    criando funções para executar as validações e mostrar as mensagens de erro

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 class Validator {
   constructor () {
     this.validator = [
-      
+      "data-min-length",
     ];
   }
   
@@ -10,8 +10,31 @@ class Validator {
     let inputsArray = [...inputs];
     
     inputsArray.forEach((input) => {
-      console.log(input);
-    });
+      for (let i = 0; this.validator.length > i; i++) {
+        if (input.getAttribute(this.validator[i]) != null) {
+          let method = this.validator[i].replace("data-", "").replace("-", "");
+          let value = input.getAttribute(this.validator[i]);
+          this[method](input, value);
+        }
+      }
+    }, this);
+  }
+  
+  minlength(input, minValue){
+    let inputLength = input.value.length;
+    let errorMessage = `O campo deve ter pelo menos ${minValue} caracteres`;
+    
+    if (inputLength < minValue) {
+      this.printMessage(input, errorMessage);
+    }
+  }
+  
+  printMessage(input, msg){
+    let template = document.querySelector(".error-validation").cloneNode();
+    template.textContent = msg;
+    let inputParent = input.parentNode;
+    template.classList.remove("template");
+    inputParent.appendChild(template);
   }
 }
 
